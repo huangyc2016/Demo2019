7a47dabe856f169bc4e2055f8ea5f0b34c4e0edc
hyc <hyc@DESKTOP-B174AQ1>
2020-08-07 18:03:55 +0800

    增加

---
+++ FluentSchedulerApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/// <summary>
+/// FluentScheduler版本5.5.0
+/// </summary>
+namespace FluentSchedulerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ QuartzApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/// <summary>
+/// Quartz版本3.1.0
+/// </summary>
+namespace QuartzApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
