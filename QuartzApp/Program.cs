ce8b148da1153db22a5d59ecd4381673a6f7b642
hyc <hyc@DESKTOP-B174AQ1>
2020-08-10 15:12:49 +0800

    TIAOZ

--- QuartzApp/Program.cs
+++ QuartzApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NLog;
+using System;
 
 /// <summary>
 /// Quartz版本3.1.0
@@ -7,9 +8,18 @@ namespace QuartzApp
 {
     class Program
     {
+        public static readonly Logger loggerError = LogHelper.GetLogger("ErrorLog", true);
+        public static readonly Logger loggerInfo = LogHelper.GetLogger("InfoLog", true);
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            loggerInfo.Info("启动服务");
+            //初始化Hander
+            InitSchedule.InitHander().GetAwaiter().GetResult();
+
+            Console.WriteLine("Press any key to close the application");
+            Console.ReadKey();
         }
     }
 }
---
+++ QuartzApp/Worker/Demo/JobB.cs
@@ -0,0 +1,40 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuartzApp.Worker.Demo
+{
+    /// <summary>
+    /// JobB
+    /// DisallowConcurrentExecution表示(不能重复执行)
+    /// </summary>
+    [DisallowConcurrentExecution]
+    public class JobB : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            Program.loggerInfo.Info("服务B开始执行");
+            //具体操作
+            try
+            {
+                await Do();
+            }
+            catch (Exception ex)
+            {
+                Program.loggerError.Error($"服务B执行报错:{ex.ToString()}");
+            }
+
+            Program.loggerInfo.Info("服务B结束执行");
+        }
+
+        private async Task Do()
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                await Console.Out.WriteLineAsync($"服务B取执行输出: {i.ToString()}");
+            }
+        }
+    }
+}
